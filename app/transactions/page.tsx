6e083e1a3b02f7c6459d15108c04f3875d7f3742
Igor Henrik <igorhenrikg1@gmail.com>
2024-11-08 18:08:04 -0300

    feat: add navbar component

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -4,6 +4,7 @@ import { transactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import Navbar from "../_components/navbar";
 
 const TransactionsPage = async () => {
   const { userId } = await auth();
@@ -13,13 +14,16 @@ const TransactionsPage = async () => {
   const transactions = await db.transaction.findMany({});
 
   return (
-    <div className="space-y-6 p-6">
-      <div className="flex w-full items-center justify-between">
-        <h1 className="text-2xl font-bold">Transações</h1>
-        <AddTransactionButton />
+    <>
+      <Navbar />
+      <div className="space-y-6 p-6">
+        <div className="flex w-full items-center justify-between">
+          <h1 className="text-2xl font-bold">Transações</h1>
+          <AddTransactionButton />
+        </div>
+        <DataTable columns={transactionColumns} data={transactions} />
       </div>
-      <DataTable columns={transactionColumns} data={transactions} />
-    </div>
+    </>
   );
 };
 
